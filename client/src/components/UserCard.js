7a49437c67c78aec0f4e972de1ae4a9269255347
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-05-03 09:12:14 -0400

    added some styling

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -5,7 +5,7 @@ function UserCard({user}) {
     return (
         <div className='userCard'>
             <Link to={`/users/${user.id}`}>
-                <h1>Full Name: {user.first_name} {user.last_name}</h1>
+                <h1>{user.first_name} {user.last_name}</h1>
                 <h1>Username: {user.user_name}</h1>
             </Link>
         </div>
